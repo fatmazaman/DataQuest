1ceb32d2b2a5275675bcabdb366117b8d7638d0c
fatmazaman <fatma302@gmail.com>
2015-03-28 13:03:56 -0400

    first project

---
+++ crime_rate.py
@@ -0,0 +1,15 @@
+# Find the lowest crime rate in U.S
+f = open('crime_rates.csv', 'r')
+data = f.read()
+rows = data.split('\n')
+full_data = []
+for row in rows:
+    split_row = row.split(",")
+    split_row[1] = int(split_row[1])
+    full_data.append(split_row)
+
+city = ""
+for item in full_data:
+    if item[1] == 130:
+        city = item[0]
+print city        
