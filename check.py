b845630516a4823f6f53b18c8d948198d4918c61
fatmazaman <fatma302@gmail.com>
2015-04-10 22:14:23 -0400

    Spell checker

---
+++ check.py
@@ -0,0 +1,3 @@
+#open the txt file
+story = open("story.txt", 'r').read()
+print story
---
+++ weather_pattern.py
@@ -0,0 +1,31 @@
+#open the file and read the contents
+
+f = open('la_weather.csv','r')
+data = f.read()
+rows = data.split('\n')
+
+# store the value of file in list
+weather_data = []
+for row in rows:
+	split_row  = row.split(',')
+	weather_data.append(split_row)
+	
+#store only second colum on list
+weather_column = []
+for item in weather_data:
+    weather_column.append(item[1])	
+
+#remove header
+weather = weather_column[1:]    
+
+# count the weather 
+weather_counts = {}
+for item in weather:
+    if item in weather_counts:
+        weather_counts[item] = weather_counts[item] + 1
+    else:
+        weather_counts[item] = 1
+print (weather_counts)     
+
+
+   
